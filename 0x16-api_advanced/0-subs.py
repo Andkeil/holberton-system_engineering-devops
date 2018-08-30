8a3d375561b0e28a6cf969b437da33825dea9aaa
Andrew Keil <monkeyninja147@gmail.com>
2018-08-30 18:06:16 +0000

    Initial

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Return number of subscribers
+to subreddit
+"""
+import json
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    url ="http://reddit.com/r/{}/about/.json".format(sys.argv[1])
+    req = requests.get(url, headers={'User-agent': 'Holbietest'})
+    return (int(req.json().get("data").get("subscribers")))

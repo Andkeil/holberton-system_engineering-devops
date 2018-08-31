cfe28a01b941038a2373a41e61ac833e68b46fc9
Andrew Keil <monkeyninja147@gmail.com>
2018-08-31 00:11:13 +0000

    pep8 fixes

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -10,15 +10,15 @@ import sys
 
 
 def recurse(subreddit, hot_list=[], after=""):
-    url = "https://www.reddit.com/r/{}/hot.json?after={}".format\
-          (subreddit, after)
+    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit,
+                                                                 after)
     req = requests.get(url, headers={'User-agent': 'Holbietest'})
     try:
         links = req.json().get("data").get("children")
         if not links:
             return None
         after = req.json().get("data").get("after")
-        if after == None:
+        if after is None:
             return hot_list
         for link in links:
             hot_list += (link.get("data").get("title"))

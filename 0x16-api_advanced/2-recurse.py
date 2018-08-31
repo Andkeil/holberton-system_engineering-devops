af5a4c50bc2ddaa3d6e941d78b7ac4c61dca4439
Andrew Keil <monkeyninja147@gmail.com>
2018-08-31 01:01:01 +0000

    Fixed to not extraneously loop

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -10,18 +10,18 @@ import sys
 
 
 def recurse(subreddit, hot_list=[], after=""):
-    url = "https://www.reddit.com/r/{}/hot.json?after={}&limit=100".\
-          format(subreddit, after)
+    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit,
+                                                                 after)
     req = requests.get(url, headers={'User-agent': 'Holbietest'})
     try:
         links = req.json().get("data").get("children")
         if not links:
             return None
         after = req.json().get("data").get("after")
-        if after is None:
-            return hot_list
         for link in links:
-            hot_list += (link.get("data").get("title"))
+            hot_list.append(link.get("data").get("title"))
+        if not after:
+            return hot_list
         return recurse(subreddit, hot_list, after)
     except Exception:
         return None
